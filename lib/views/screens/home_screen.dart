81d12ff553870d220de0cfae5678203cceb08cfd
Mike Mambwe <mikeapple056@gmail.com>
2025-02-24 21:59:05 +0200

    home_screen update

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white ,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
