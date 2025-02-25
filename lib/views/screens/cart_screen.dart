bbbdddc4c1e650a7cc2a1b842895047b3ffd0535
Mike Mambwe <mikeapple056@gmail.com>
2025-02-25 19:28:14 +0200

    alignment on the cart screen

--- lib/views/screens/cart_screen.dart
+++ lib/views/screens/cart_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:point_on_sale_system/providers/cart_provider.dart';
-import 'package:point_on_sale_system/views/pages/home_page.dart';
 import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
@@ -16,151 +15,142 @@ class CartScreen extends StatelessWidget {
           ? Center(child: Text('Cart Is Empty'))
           : Column(
               children: [
-                SizedBox(
-                  height: 70,
-                ),
+                SizedBox(height: 70),
                 Padding(
-                    padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomePage(),
-                              ),
-                            );
-                          },
-                          child: Row(
-                            children: [
-                              FaIcon(FontAwesomeIcons.angleLeft),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                'Checkout',
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w500),
-                              ),
-                            ],
+                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          FaIcon(FontAwesomeIcons.angleLeft),
+                          SizedBox(width: 10),
+                          Text(
+                            'Checkout',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
                           ),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            cartProvider
-                                .removeFromCart(cartProvider.cartItems.last);
-                          },
-                          icon: FaIcon(
-                            FontAwesomeIcons.trashCan,
-                          ),
-                        ),
-                      ],
-                    )),
-                SizedBox(
-                  height: 30,
+                        ],
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          cartProvider
+                              .removeFromCart(cartProvider.cartItems.last);
+                        },
+                        icon: FaIcon(FontAwesomeIcons.trashCan),
+                      ),
+                    ],
+                  ),
                 ),
+                SizedBox(height: 30),
                 Expanded(
                   child: ListView.builder(
                     itemCount: cartItem.length,
                     itemBuilder: (context, index) {
                       final items = cartItem[index];
                       return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: ListTile(
-                          leading: Image.asset(items.image),
-                          title: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                items.name,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500, fontSize: 18),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Row(
-                                children: [
-                                  Container(
-                                    height: 30,
-                                    width: 30,
-                                    decoration: BoxDecoration(
-                                      color: Colors.pink,
-                                      shape: BoxShape.circle,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 18.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // Left side: Image & Name + Quantity Controls
+                            Row(
+                              children: [
+                                Image.asset(
+                                  items.image,
+                                  width: 60,
+                                  height: 60,
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(width: 15),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      items.name,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 18),
                                     ),
-                                    child: Center(
-                                      child: FaIcon(
-                                        FontAwesomeIcons.subtract,
-                                        color: Colors.white,
-                                        size: 16,
-                                      ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        Container(
+                                          height: 30,
+                                          width: 30,
+                                          decoration: BoxDecoration(
+                                            color: Colors.pink,
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: Center(
+                                            child: FaIcon(
+                                              FontAwesomeIcons.subtract,
+                                              color: Colors.white,
+                                              size: 16,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          '0',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                        SizedBox(width: 10),
+                                        Container(
+                                          height: 30,
+                                          width: 30,
+                                          decoration: BoxDecoration(
+                                            color: Colors.pink,
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: Center(
+                                            child: FaIcon(
+                                              FontAwesomeIcons.plus,
+                                              color: Colors.white,
+                                              size: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    '0',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.w600),
-                                  ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Container(
-                                    height: 30,
-                                    width: 30,
-                                    decoration: BoxDecoration(
-                                      color: Colors.pink,
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: Center(
-                                      child: FaIcon(
-                                        FontAwesomeIcons.plus,
-                                        color: Colors.white,
-                                        size: 16,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                          trailing: Column(
-                            children: [
-                              Text("\$${items.price.toStringAsFixed(2)}",
+                                  ],
+                                ),
+                              ],
+                            ),
+                            // Right side: Price & Delete Icon
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "\$${items.price.toStringAsFixed(2)}",
                                   style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 18,
-                                  )),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Container(
-                                height: 30,
-                                width: 30,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    color: Colors.grey[500]!, // Border color
-                                    width: 3.0, // Border thickness
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18),
+                                ),
+                                SizedBox(height: 10),
+                                Container(
+                                  height: 28,
+                                  width: 28,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: Colors.grey[500]!,
+                                      width: 2.5,
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: FaIcon(
+                                      FontAwesomeIcons.close,
+                                      color: Colors.grey[500],
+                                      size: 16,
+                                    ),
                                   ),
                                 ),
-                                child: Center(
-                                  child: FaIcon(
-                                    FontAwesomeIcons.close,
-                                    color: Colors.grey[500],
-                                    size: 16,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                              ],
+                            ),
+                          ],
                         ),
                       );
                     },
